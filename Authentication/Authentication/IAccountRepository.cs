e25b22047d2fbe15d83cf9b93b836f0676672cb2
Ross Merrigan <email@rossmerrigan.com>
2012-12-15 15:05:59 +0000

    fix the repository

--- Authentication/Authentication/IAccountRepository.cs
+++ Authentication/Authentication/IAccountRepository.cs
@@ -4,7 +4,7 @@ using MvcContrib.Pagination;
 
 namespace Authentication
 {
-    public interface IAccountRepository<TUser, TUserList> where TUser : IUser
+    public interface IAccountRepository<TUser, TUserList, TId> where TUser : IUser<TId>
     {
         IPagination<TUserList> GetPagedList(int pageIndex, int pageSize);
 
@@ -17,5 +17,9 @@ namespace Authentication
         IEnumerable<TUser> GetByEmailAddress(string email, int pageIndex, int pageSize);
         IEnumerable<TUser> GetByUsername(string username, int pageIndex, int pageSize);
         IEnumerable<TUser> GetByUsernameAndQuestionAnswer(string username, string encodePasswordQuestionAnswer);
+        void Save(TUser obj);
+        void Delete(TUser obj);
+        IEnumerable<TUser> FindAll();
+        TUser FindOne(TId id);
     }
 }
